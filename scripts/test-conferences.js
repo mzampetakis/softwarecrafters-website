softwarecrafters/website

build: modernise to use vite

scripts/test-conferences.js
@@ -1,23 +1,24 @@
-const glob = require("glob");
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
-const Ajv = require("ajv");
-const communitySchema = require("../conferences_schema.json");
+const Ajv = require('ajv');
+const conferenceSchema = require('../conferences_schema.json');
 
-const ajv = new Ajv({ schemaId: "id" });
-ajv.addMetaSchema(require("ajv/lib/refs/json-schema-draft-07.json"));
+const ajv = new Ajv({ schemaId: 'id' });
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-07.json'));
 
-const validate = ajv.compile(communitySchema);
+const validate = ajv.compile(conferenceSchema);
 
-const conferenceFiles = glob.sync(
-  path.resolve(__dirname, "../conferences/") + "/*.json"
-);
+const conferencesDir = path.resolve(__dirname, '../conferences/');
+const conferenceFiles = fs
+  .readdirSync(conferencesDir)
+  .filter((file) => file.endsWith('.json'))
+  .map((file) => path.join(conferencesDir, file));
 
 let failed = false;
 
-console.log("Testing conference files");
-conferenceFiles.forEach(file => {
+console.log('Testing conference files');
+conferenceFiles.forEach((file) => {
   const baseName = path.basename(file);
   const isValid = validate(JSON.parse(fs.readFileSync(file)));
 
@@ -25,7 +26,7 @@ conferenceFiles.forEach(file => {
     failed = true;
     console.error(`X ${baseName}`);
     console.error(JSON.stringify(validate.errors, undefined, 2));
-    console.error("\n");
+    console.error('\n');
   } else {
     console.log(`✓ ${baseName}`);
   }

mzampetakis/softwarecrafters-website

scripts/test-conferences.js
@@ -1,22 +1,25 @@
-const glob = require('glob');
 const path = require('path');
 const fs = require('fs');
 
 const Ajv = require('ajv/dist/2020');
 const addFormats = require('ajv-formats');
-const communitySchema = require('../conferences_schema_v2.json');
+const conferenceSchema = require('../conferences_schema_v2.json');
 
 const ajv = new Ajv();
 addFormats(ajv);
 
-const validate = ajv.compile(communitySchema);
+const validate = ajv.compile(conferenceSchema);
 
-const conferenceFiles = glob.sync(path.resolve(__dirname, '../conferences/') + '/*.json');
+const conferencesDir = path.resolve(__dirname, '../conferences/');
+const conferenceFiles = fs
+  .readdirSync(conferencesDir)
+  .filter((file) => file.endsWith('.json'))
+  .map((file) => path.join(conferencesDir, file));
 
 let failed = false;
 
 console.log('Testing conference files');
-conferenceFiles.forEach(file => {
+conferenceFiles.forEach((file) => {
   const baseName = path.basename(file);
   const isValid = validate(JSON.parse(fs.readFileSync(file)));
 